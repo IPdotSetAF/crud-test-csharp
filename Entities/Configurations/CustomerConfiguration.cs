c036fb0416f715117c540ba4137c1bc14a40e619
mah19997822@gmail.com <mah19997822@gmail.com>
2022-10-18 16:42:42 +0330

    seeded test customer data added new migration

--- Entities/Configurations/CustomerConfiguration.cs
+++ Entities/Configurations/CustomerConfiguration.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,7 +14,55 @@ namespace Entities.Configurations
     {
         public void Configure(EntityTypeBuilder<Customer> builder)
         {
-            //TODO: Seed Customer data
+            builder.HasData(
+                new Customer
+                {
+                    Id = SeededGuid(1),
+                    FirstName ="Ali",
+                    LastName ="Nazari",
+                    PhoneNumber = 989387016860,
+                    BankAccountNumber=1212121212121212,
+                    Email="ipdotsetaf.work@gmail.com",
+                    DateOfBirth= DateTime.Now
+                },
+                new Customer
+                {
+                    Id = SeededGuid(2),
+                    FirstName = "Mahdi",
+                    LastName = "Nazari",
+                    PhoneNumber = 989387016860,
+                    BankAccountNumber = 1212121212121212,
+                    Email = "ipdotsetaf.work@gmail.com",
+                    DateOfBirth = DateTime.Now
+                },
+                new Customer
+                {
+                    Id = SeededGuid(3),
+                    FirstName = "Saeed",
+                    LastName = "Rezaii",
+                    PhoneNumber = 19387016860,
+                    BankAccountNumber = 1212121212121212,
+                    Email = "ipdotsetaf.work@gmail.com",
+                    DateOfBirth = DateTime.Now
+                },
+                new Customer
+                {
+                    Id = SeededGuid(4),
+                    FirstName = "Hooshang",
+                    LastName = "Motahari",
+                    PhoneNumber = 989387016860,
+                    BankAccountNumber = 1212121212121212,
+                    Email = "ipdotsetaf.work@gmail.com",
+                    DateOfBirth = DateTime.Now
+                }
+            ); ;
+        }
+
+        private static Guid SeededGuid(int seed)
+        {
+            byte[] bytes = new byte[16];
+            BitConverter.GetBytes(seed).CopyTo(bytes, 0);
+            return new Guid(MD5.HashData(bytes));
         }
     }
 }
---
+++ Entities/Migrations/20221018130016_seed_customer_data.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Entities.Migrations
+{
+    public partial class seed_customer_data : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Customer",
+                columns: new[] { "Id", "BankAccountNumber", "DateOfBirth", "Email", "FirstName", "LastName", "PhoneNumber" },
+                values: new object[,]
+                {
+                    { new Guid("3dde6799-7063-d42e-ba04-85ff4e664534"), 1212121212121212m, new DateTime(2022, 10, 18, 16, 30, 16, 289, DateTimeKind.Local).AddTicks(4883), "ipdotsetaf.work@gmail.com", "Hooshang", "Motahari", 989387016860m },
+                    { new Guid("5c740a82-5dab-dd83-8452-e4299aaad12f"), 1212121212121212m, new DateTime(2022, 10, 18, 16, 30, 16, 289, DateTimeKind.Local).AddTicks(4859), "ipdotsetaf.work@gmail.com", "Mahdi", "Nazari", 989387016860m },
+                    { new Guid("b2c3a9ba-f1a9-2627-dce1-c0101ddfff08"), 1212121212121212m, new DateTime(2022, 10, 18, 16, 30, 16, 289, DateTimeKind.Local).AddTicks(4871), "ipdotsetaf.work@gmail.com", "Saeed", "Rezaii", 19387016860m },
+                    { new Guid("ff336907-0499-11d1-0d89-6e2c525e39e5"), 1212121212121212m, new DateTime(2022, 10, 18, 16, 30, 16, 289, DateTimeKind.Local).AddTicks(4817), "ipdotsetaf.work@gmail.com", "Ali", "Nazari", 989387016860m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Customer",
+                keyColumn: "Id",
+                keyValue: new Guid("3dde6799-7063-d42e-ba04-85ff4e664534"));
+
+            migrationBuilder.DeleteData(
+                table: "Customer",
+                keyColumn: "Id",
+                keyValue: new Guid("5c740a82-5dab-dd83-8452-e4299aaad12f"));
+
+            migrationBuilder.DeleteData(
+                table: "Customer",
+                keyColumn: "Id",
+                keyValue: new Guid("b2c3a9ba-f1a9-2627-dce1-c0101ddfff08"));
+
+            migrationBuilder.DeleteData(
+                table: "Customer",
+                keyColumn: "Id",
+                keyValue: new Guid("ff336907-0499-11d1-0d89-6e2c525e39e5"));
+        }
+    }
+}
