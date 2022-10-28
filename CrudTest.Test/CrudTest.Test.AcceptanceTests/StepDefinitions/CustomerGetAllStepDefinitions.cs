d858c086cc13d2313f1007caf71f05a3b485f60f
mah19997822@gmail.com <mah19997822@gmail.com>
2022-10-28 22:15:25 +0330

    completed getallCustomers and getCustomerById steps

--- CrudTest.Test/CrudTest.Test.AcceptanceTests/StepDefinitions/CustomerGetAllStepDefinitions.cs
+++ CrudTest.Test/CrudTest.Test.AcceptanceTests/StepDefinitions/CustomerGetAllStepDefinitions.cs
@@ -1,21 +1,31 @@
+using CrudTest.Core.Contracts.DTOs.Customer;
+using CrudTest.Test.SharedMocks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using TechTalk.SpecFlow;
+using Xunit;
 
 namespace CrudTest.Test.AcceptanceTests.StepDefinitions
 {
     [Binding]
     public class CustomerGetAllStepDefinitions
     {
+        private ObjectResult _result;
+
         [When(@"I request to get all customers")]
         public void WhenIRequestToGetAllCustomers()
         {
-            throw new PendingStepException();
+            _result = (ObjectResult)MockCustomerController.CustomerController.GetAllCustomers().Result;
         }
 
         [Then(@"the action should return a list of all customers with Status code (.*)")]
         public void ThenTheActionShouldReturnAListOfAllCustomersWithStatusCode(int statusCode)
         {
-            throw new PendingStepException();
+            Assert.NotNull(_result);
+            Assert.Equal(statusCode, _result.StatusCode);
+            Assert.IsAssignableFrom<IEnumerable<CustomerGetDTO>>(_result.Value);
+            Assert.NotEmpty(_result.Value as IEnumerable<CustomerGetDTO>);
         }
     }
 }
--- CrudTest.Test/CrudTest.Test.AcceptanceTests/StepDefinitions/CustomerGetByIdStepDefinitions.cs
+++ CrudTest.Test/CrudTest.Test.AcceptanceTests/StepDefinitions/CustomerGetByIdStepDefinitions.cs
@@ -1,27 +1,47 @@
+using CrudTest.Core.Contracts.DTOs;
+using CrudTest.Core.Contracts.DTOs.Customer;
+using CrudTest.Core.Contracts.Utils;
+using CrudTest.Test.SharedMocks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.CommonModels;
 
 namespace CrudTest.Test.AcceptanceTests.StepDefinitions
 {
     [Binding]
     public class CustomerGetByIdStepDefinitions
     {
+        private ObjectResult _result;
+        private Guid _id;
+
         [Given(@"the Id \((.*)\) of a customer")]
         public void GivenTheIdOfACustomer(int idSeed)
         {
-            throw new PendingStepException();
+            _id = GuidUtil.SeededGuid(idSeed);
         }
 
         [When(@"I request to get the customer by that Id")]
         public void WhenIRequestToGetTheCustomerByThatId()
         {
-            throw new PendingStepException();
+            _result = (ObjectResult)MockCustomerController.CustomerController.GetCustomer(_id).Result;
         }
 
         [Then(@"the action returns customer with Status code \((.*)\)")]
         public void ThenTheActionReturnsCustomerWithStatusCode(int statusCode)
         {
-            throw new PendingStepException();
+            Assert.NotNull(_result);
+            Assert.Equal(statusCode, _result.StatusCode);
+            if (statusCode == 200)
+            {
+                Assert.Equal((_result.Value as CustomerGetDTO).Id, _id);
+                Assert.IsAssignableFrom<CustomerGetDTO>(_result.Value);
+            }
+            else
+            {
+                Assert.IsAssignableFrom<ErrorDTO>(_result.Value);
+            }
         }
     }
 }
